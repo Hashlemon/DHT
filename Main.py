688182a813c84307439b1a13a663e5efcb40f9e9
Your Name <you@example.com>
2019-12-27 23:06:44 +0300

    DHT

---
+++ Main.py
@@ -0,0 +1,65 @@
+from tkinter import Tk, Frame, Label, Button
+import RPi.GPIO as GPIO
+import dht11
+import time
+import datetime
+
+
+class Main(Frame):
+    # initialize GPIO
+    GPIO.setwarnings(True)
+    GPIO.setmode(GPIO.BCM)
+
+    # read data using pin 14, 15
+    instance_1 = dht11.DHT11(pin=14)
+    instance_2 = dht11.DHT11(pin=15)
+
+    temp = "0"
+    hum = "0"
+    def __init__(self, root):
+        super().__init__(root)
+        self.build()
+
+    def get_sample(self):
+
+        result = self.instance_1.read()
+        if result.is_valid():
+                print("Last valid input: " + str(datetime.datetime.now()))
+
+                print("Temperature: %-3.1f C" % result.temperature)
+                print("Humidity: %-3.1f %%" % result.humidity)
+
+
+    def build(self):
+
+        self.lbl_t = Label(
+            text=self.temp,
+            font=("Times New Roman", 21, "bold"),
+            bg="#000",
+            foreground="#FFF",
+        )
+        self.lbl_t.place(x=11, y=30)
+
+        self.lbl_h = Label(
+            text=self.hum,
+            font=("Times New Roman", 21, "bold"),
+            bg="#000",
+            foreground="#FFF",
+        )
+        self.lbl_h.place(x=11, y=80)
+
+
+        self.btn = Button(
+            text="Get sample", bg="#FFF", font=("Times New Roman", 15), command=lambda: self.get_sample()).place(x=10, y=150, width=120, height=50)
+
+
+
+if __name__ == "__main__":
+    root = Tk()
+    root["bg"] = "#000"
+    root.title("DHT11")
+    root.geometry("485x550+200+200")
+    root.resizable(False, False)
+    app = Main(root)
+    app.pack()
+    root.mainloop()
